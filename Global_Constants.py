dd8340dcd80367c2b466f9080780bc1f8477327e
jharris1993 <jimrh@charter.net>
2020-11-30 19:49:11 +0300

    Added

---
+++ Global_Constants.py
@@ -0,0 +1,46 @@
+#  Global Remote Camera Robbot Constants
+#
+#  This file, when included, should provide the basic initialization
+#  Of the global constants and structures needed for the
+#  Remote Camera Robot program files
+#
+#  The object of this file is to provide a uniform definition
+#  of all the global structures used in the New Remote Camera
+#  Robot project.  This way, there is one, and only one, location
+#  that, when edited, automagically propigates to all affected
+#  procedures
+#
+##############################
+### Basic Global Constants ###
+##############################
+
+MAX_FORCE = 5.0
+MIN_SPEED = 0.0       # forces a minimum speed if force > 0
+MAX_SPEED = 500.0
+force_multiplier = 1  # allows a slower, smoother startup if > 1
+drive_constant = (MAX_SPEED - MIN_SPEED) / (force_multiplier * MAX_FORCE)
+
+# calibration constants for the servo center position which are
+# determined experimentally by visual inspection of the servos
+# TODO: Create a servo calibration routine that is a part of the
+#       control panel and saves these settings in the gpg3_config.json file.
+#       This will allow these calibraton constants to be globally
+#       available to any process that wants to use them.
+#
+# Initially the vposition and hposition of the two servos
+# is set to vcenter and hcenter respectively, then hposition and vposition
+# are incremented/decremented to move the servos as commanded
+
+vcenter = vposition = 93  # tilt charlie's head up slightly
+hcenter = hposition = 93
+
+# Set the movement step size
+servo_step_size = 5
+
+# Directory Path can change depending on where you install this file.  Non-standard installations
+# may require you to change this directory.
+directory_path = '/home/pi/Dexter/GoPiGo3/Projects/RemoteCameraRobot/static'
+
+##################################
+### End Basic Global Constants ###
+##################################
---
+++ Head_Motion.py
@@ -0,0 +1,55 @@
+#  File Head_Motion
+#
+#  This file provides the global routines for creating
+#  instances of the servo class and the functions needed
+#  to move the robots head in whatever direction is needed
+#
+
+#  Add instantiate "servo" object
+servo1 = gopigo3_robot.init_servo('SERVO1')
+servo2 = gopigo3_robot.init_servo('SERVO2')
+
+#  Generic "head movement" routine
+def move_head(hpos, vpos):
+    servo1.rotate_servo(hpos)
+    servo2.rotate_servo(vpos)
+    sleep(0.25)
+    servo1.disable_servo()
+    servo2.disable_servo()
+    return(0)
+
+# Center Charlie's head
+def center_head():
+    global vposition
+    global vcenter
+    global hposition
+    global hcenter
+
+    vposition = vcenter
+    hposition = hcenter
+    move_head(hposition, vposition)
+    return(0)
+
+# Shake Charlie's head - just to prove he's alive! ;)
+def shake_head():
+    vpos = 88
+    hpos = 97
+
+    logging.info("Shaking Charlie's Head From Side To Side\n")
+    hpos = 110
+    move_head(hpos, vpos)
+    hpos = 84
+    move_head(hpos, vpos)
+
+    logging.info("Centering Charlie's head horizontally\n")
+    center_head()
+
+    logging.info("Moving Charlie's Head Up And Down\n")
+    vpos = 110
+    move_head(hpos, vpos)
+    vpos = 66
+    move_head(hpos, vpos)
+
+    logging.info("Re-centering Charlie's head vertically\n")
+    center_head()
+    return(0)
