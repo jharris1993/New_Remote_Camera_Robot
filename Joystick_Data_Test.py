cd62593d6c865339e33a00c7feb101f7c480c1c5
Jim Harris <jimrh@charter.net>
2022-02-05 04:23:15 +0300

    Updated comment spacing and added explanatory text

--- Joystick_Data_Test.py
+++ Joystick_Data_Test.py
@@ -1,10 +1,10 @@
 #
-# Dexter Industries GoPiGo3 Remote Camera robot
-# With this project you can control your Raspberry Pi Robot, the GoPiGo3, with a phone, tablet, or browser.
-# Remotely view your robot as first person in your browser.
+#  Dexter Industries GoPiGo3 Remote Camera robot
+#  With this project you can control your Raspberry Pi Robot, the GoPiGo3, with a phone, tablet, or browser.
+#  Remotely view your robot as first person in your browser.
 #
-# You MUST run this with python3
-# To Run:  python3 flask_server.py
+#  You MUST run this with python3
+#  To Run:  python3 flask_server.py
 #
 #  Modified by Jim Harris to eliminate nipple.js and allow robot control with a standard joystick.
 #  Please direct support requests to user "jimrh" at https://forum.dexterindustries.com
@@ -20,16 +20,16 @@ sys.path.insert(0, '/home/pi/Project_Files/Projects/GoPiGo3/Software/Python')
 
 from werkzeug.datastructures import ResponseCacheControl
 
-# check if it's ran with Python3
+#  check if it's ran with Python3
 assert sys.version_info[0:1] == (3,)
 
-# imports needed for web server
+#  imports needed for web server
 from flask import Flask, jsonify, render_template, request, Response, send_from_directory, url_for
 from werkzeug.serving import make_server
 from gopigo3 import FirmwareVersionError
 from easygopigo3 import EasyGoPiGo3
 
-# imports needed for stream server
+#  imports needed for stream server
 import io
 import picamera
 import socketserver
@@ -51,22 +51,22 @@ app = Flask(__name__, static_url_path='')
 force = float(0.00)
 normal_speed = int(150)  #  Max speed if the trigger is pressed half-way
 turbo_speed = int(300)  #  Max speed if the trigger is fully pressed
-speed = int(0)  # this represents the currently selected maximum, either normal or turbo speed
+speed = int(0)  #  this represents the currently selected maximum, either normal or turbo speed
 desired_speed = int(0)  #  This is the adjusted speed based on joystick force.
 vcenter = vposition = int(92)  #  The "calibrated" positions for Charlie's head 
 hcenter = hposition = int(88)  #  to be centered in both axes.
 
-# Set the movement step size
+#  Set the movement step size
 servo_step_size = int(5)
 
-# Directory Path can change depending on where you install this file.  Non-standard installations
-# may require you to change this directory.
+#  Directory Path can change depending on where you install this file.  Non-standard installations
+#  may require you to change this directory.
 #
-# If you install this in directory "x", both "static" and "templates"
-# should be subdirectories of directory "x".
-# Example: This file is placed in /home/pi/project. Then you should place both
-# "static" and "templates" one directory below it - /home/pi/project/templates and
-# /home/pi/project/static
+#  If you install this in directory "x", both "static" and "templates"
+#  should be subdirectories of directory "x".
+#  Example: This file is placed in /home/pi/project. Then you should place both
+#  "static" and "templates" one directory below it - /home/pi/project/templates and
+#  /home/pi/project/static
 #
 #  TODO:  Figure out how to make this self-referencing so that the user can put this wherever he wants.
 directory_path = '/home/pi/Project_Files/Projects/New_Remote_Camera_Robot/static'
@@ -75,7 +75,7 @@ directory_path = '/home/pi/Project_Files/Projects/New_Remote_Camera_Robot/static
 ### End Basic Global Constants ###
 ##################################
 
-# for triggering the shutdown procedure when a signal is detected
+#  for triggering the shutdown procedure when a signal is detected
 keyboard_trigger = Event()
 def signal_handler(signal, frame):
     logging.info('Signal detected. Stopping threads.')
@@ -121,7 +121,7 @@ def move_head(hpos, vpos):
     servo2.disable_servo()
     return(0)
 
-# Center Charlie's head
+#  Center Charlie's head
 def center_head():
     global vposition
     global vcenter
@@ -133,7 +133,7 @@ def center_head():
     move_head(hposition, vposition)
     return(0)
 
-# Shake Charlie's head - just to prove he's alive! ;)
+#  Shake Charlie's head - just to prove he's alive! ;)
 def shake_head():
     vposition = vcenter
     hpos = hcenter
@@ -176,7 +176,7 @@ def create_CORS_response():
 
 @app.route("/robot", methods = ["POST"])
 def get_args():
-    # get the query
+    #  get the query
     args = request.args
 
 #  Print out the received values of the arg-list so I can verify they're correct.
@@ -198,9 +198,9 @@ def get_args():
     #  Force cach clearing
     #  Note: expires = 0 usually won't work
     #  To prevent caching, set to a past date
-    resp.headers["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
-    resp.headers["Pragma"] = "no-cache" # HTTP 1.0.
-    resp.headers["Expires"] = "Wed, 21 Oct 2015 07:28:00 GMT" # Proxies.
+    resp.headers["Cache-Control"] = "no-cache, no-store, must-revalidate" #  HTTP 1.1.
+    resp.headers["Pragma"] = "no-cache" #  HTTP 1.0.
+    resp.headers["Expires"] = "Wed, 21 Oct 2015 07:28:00 GMT" #  Proxies.
     resp.mimetype = "application/json"
     resp.status = "OK"
     resp.status_code = 200
@@ -226,7 +226,7 @@ def send_static(path):
 #   need while running the robot as a fraction/percentage of
 #   speed vs force or desired_speed vs the x_axis deflection.
 #
-#   Desired_speed is the fraction of the currently allowable maximum speed, (speed, either normal or turbo)
+#   Desired_speed is the fraction of the currently allowable maximum speed, (either normal or turbo)
 #   represented by the deflection of the joystick, either forward or backwards.
 #   If the robot is moving ahead or backwards, this is the speed of both wheels
 #   If the robot is turning, this is the speed of the outside wheel.
@@ -252,9 +252,9 @@ def calc_differential_speed(desired_speed, x_axis):
         differential_speed = desired_speed
     return (differential_speed)
 
-# Implement "correct" (away from zero) rounding for both
-# positive and negative numbers
-# ref: https://www.pythontutorial.net/advanced-python/python-rounding/
+#  Implement "correct" (away from zero) rounding for both
+#  positive and negative numbers
+#  ref: https://www.pythontutorial.net/advanced-python/python-rounding/
 
 def round_up(x):
     x = 100 * x  #  This will allow two decimal digits of precision ranging from zero to one.
@@ -292,7 +292,7 @@ def process_robot_commands(args):
 
 #  This reduces the x_axis sensitivity
 #  Select a number that allows the x_axis to do what is necessary,
-#  without undue "toouchyness"
+#  without undue "touchyness"
     x_axis = x_axis * 0.50  #  reduces sensitivity by a factor of 2.
 
 #  Enable "Turbo" speed
@@ -301,8 +301,8 @@ def process_robot_commands(args):
     else:
         speed = normal_speed
 
-    # Insist on sane values
-    if (abs(x_axis)) < 0.10: # provide a little bit of dead-zone for the x_axis
+    #  Insist on sane values
+    if (abs(x_axis)) < 0.10: #  provide a little bit of dead-zone for the x_axis
         x_axis = 0
 
     if x_axis > 1:
@@ -327,6 +327,7 @@ def process_robot_commands(args):
 ############################
 ##    Motion Selection    ##
 ############################
+#
 #  Depending on the position of the x and y axes
 #  and the state of trigger_1, we determine
 #  **IF** the robot should be moving,
@@ -337,15 +338,18 @@ def process_robot_commands(args):
         print("Robot Stopped. . .\n")
 
     elif trigger_1 == 1 and y_axis < 0:
-        # We're moving forward - either straight, left, or right.
-        print("The robot is moving forward and is ", end="")
+        #  We're moving forward - either straight, left, or right.
+        print("The robot is moving forward and is ", end="")  #  "end=" means print the next part on the same line as this
         
-        # if we're not moving directly forward, the inside wheel must be slower
-        # than the outside wheel by some percentage.
+        #  if we're not moving directly forward, the inside wheel must be slower
+        #  than the outside wheel by some percentage.
 
-        # When moving to the left, the left wheel must be moving slower than
-        # the right wheel by some percentage, depending on the sharpness of the turn.
-        # "set_motor_dps" allows the wheels to be set to individual speeds.
+        #  When moving to the left, the left wheel must be moving slower than
+        #  the right wheel by some percentage depending on the sharpness of the turn.
+        #  In order to be able to regulate the degree of sharpness of the turn with a degree of precision
+        #  We need to use "set_motor_dps" because "set_motor_dps" allows the wheels to be set to individual speeds
+        #  based on the calculated speeds for each wheel.
+
         if x_axis < 0:  #  Moving fowrard to the left
             desired_speed = int(calc_desired_speed(speed, force))
             differential_speed = int(calc_differential_speed(desired_speed, x_axis))
@@ -353,7 +357,7 @@ def process_robot_commands(args):
             my_gopigo3.set_motor_dps(my_gopigo3.MOTOR_LEFT, differential_speed)
             print("moving forward to the left\n")
 
-            # Moving to the right, we apply the same logic as before, but swap wheels.
+            #  Moving to the right, we apply the same logic as before, but swap wheels.
         elif x_axis > 0:  #  Moving fowrard to the right
             desired_speed = int(calc_desired_speed(speed, force))
             differential_speed = int(calc_differential_speed(desired_speed, x_axis))
@@ -361,28 +365,30 @@ def process_robot_commands(args):
             my_gopigo3.set_motor_dps(my_gopigo3.MOTOR_RIGHT, differential_speed)
             print("moving forward to the right\n")
 
-        else:  # Moving directly forward
+        else:  #  Moving directly forward
             desired_speed = int(calc_desired_speed(speed, force))
             my_gopigo3.set_motor_dps(my_gopigo3.MOTOR_LEFT, desired_speed)
             my_gopigo3.set_motor_dps(my_gopigo3.MOTOR_RIGHT, desired_speed)
             print("moving forward straight ahead\n")
 
     elif trigger_1 == 1 and y_axis > 0:
-        # We're moving backward
+        #
+        #  We're moving backward
+        #
         #  This is the exact same logic and calculation as moving forward
         #  Except that it's "backwards" (bad pun!)
         #  We do this by changing the sign of the speed requested.
 
-        # if we're not moving directly backward, the inside wheel must be slower
-        # than the outside wheel by some percentage.
+        #  if we're not moving directly backward, the inside wheel must be slower
+        #  than the outside wheel by some percentage.
         print("The robot is moving backward and is ", end="")
 
         #  reduce maximum reverse speed to 1/2 forward speed
         speed = speed * 0.5
 
         if x_axis < 0:  #  Moving backward to the left
-            # Moving to the left, the left wheel must be moving slower than
-            # the right wheel by some percentage.
+            #  Moving to the left, the left wheel must be moving slower than
+            #  the right wheel by some percentage.
             desired_speed = int(calc_desired_speed(speed, force))
             differential_speed = int(calc_differential_speed(desired_speed, x_axis))
             my_gopigo3.set_motor_dps(my_gopigo3.MOTOR_RIGHT, -desired_speed)
@@ -390,7 +396,7 @@ def process_robot_commands(args):
             print("moving backward to the left\n")
 
         elif x_axis > 0:  #  Moving backward to the right
-            # Moving to the right, we apply the same logic, but swap wheels.
+            #  Moving to the right, we apply the same logic, but swap wheels.
             desired_speed = int(calc_desired_speed(speed, force))
             differential_speed = int(calc_differential_speed(desired_speed, x_axis))
             my_gopigo3.set_motor_dps(my_gopigo3.MOTOR_LEFT, -desired_speed)
@@ -403,7 +409,17 @@ def process_robot_commands(args):
             my_gopigo3.set_motor_dps(my_gopigo3.MOTOR_RIGHT, -desired_speed)
             print("moving straight backward\n")
 
+
+###################################
+##    Head Movement Selection    ##
+###################################
+#
 #  If we're not receiving movement messages, maybe it's a head motion request?
+#
+#  Use "if" instead of "elif" here because the logic falls through all thge "elif's"
+#  like they were "switch-case" statements.  Here we want the arrow routines
+#  to be executed independently of the robot's motion.
+#
     if motion_state == 'ArrowUp':
         print('\nmoving head up\n')
         vposition += servo_step_size
@@ -440,13 +456,12 @@ def process_robot_commands(args):
         print(f'vposition is {vposition} - hposition is {hposition}\n')
 
     elif motion_state == 'Escape':
-        print("Shutdown command recieved from the browser.\nSending shutdown event message to the event handler.\n")
+        print("A shutdown command was recieved from the connected browser.\nSending the shutdown message to the server's event handler.\n")
         my_gopigo3.stop()
         keyboard_trigger.set()        
 
     else:
         motion_state = 'unknown'
-#        print('\nUnknown (ignored) key pressed\n')
     return
 
 
@@ -490,8 +505,8 @@ class StreamingOutput(object):
 
     def write(self, buf):
         if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
+            #  New frame, copy the existing buffer's content and notify all
+            #  clients it's available
             self.buffer.truncate()
             with self.condition:
                 self.frame = self.buffer.getvalue()
@@ -539,7 +554,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 #############################
 
 if __name__ == "__main__":
-    # registering both types of termination signals
+    #  registering both types of termination signals
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
@@ -550,7 +565,7 @@ if (os.system("sudo systemctl restart nginx")) != 0:
 else:
     print("\nThe nginx proxy/secure context wrapper service has successfully started.\nNginx is now listening for HTTPS connections on port 443.\n")
 
-# firing up the video camera (pi camera)
+#  firing up the video camera (pi camera)
     camera = picamera.PiCamera()
     output = StreamingOutput()
     camera.resolution='800x600'
@@ -563,39 +578,39 @@ else:
     sleep(0.25)
     print("The streaming camera process has started successfully.\n")
 
-    # starting the video streaming server
+    #  starting the video streaming server
     streamserver = Thread(target = stream.serve_forever)
     streamserver.start()
     sleep(0.25)
     print("The streaming server has started successfully on port", STREAM_PORT, "\n")
 
-    # starting the web server
+    #  starting the web server
     webserver = WebServerThread(app, HOST, WEB_PORT)
     webserver.start()
     sleep(0.25)
     print("The flask web server has started successfully on port", WEB_PORT, "\n")
 
-    # Shaking Charlie's head to indicate startup
+    #  Shaking Charlie's head to indicate startup
     print("Charlie is signalling that the startup propcess has successfully completed by shaking his head.\n")
     shake_head()
     sleep(0.25)  #  Give head time to get centered.
     print("Joystick_Data_Test is now ready and is listening for browser connections on ports", WEB_PORT, "amd", STREAM_PORT, "\n")
 
-    # and run the flask server untill a keyboard event is set
-    # or the escape key is pressed
+    #  and run the flask server untill a keyboard event is set
+    #  or the escape key is pressed
     while not keyboard_trigger.is_set():
         sleep(0.25)
 
-    # until some keyboard event is detected
-    print("\n ==========================\n\n")
+    #  until some keyboard event is detected
+    print(" ==========================\n")
     print("Shutdown command event received\n")
 
-    # begin shutdown procedure
+    #  begin shutdown procedure
     webserver.shutdown()
     camera.stop_recording()
     stream.shutdown()
 
-    # and finalize shutting them down
+    #  and finalize shutting them down
     webserver.join()
     streamserver.join()
     print("All web and streaming services have successfully shut down.\n")
@@ -606,10 +621,10 @@ else:
     print("The nginx proxy/secure contxt wrapper service has successfully disconnected and shut down.\n")
     sleep(0.25)
 
-    # Center Charlie's Head on shutdown
+    #  Center Charlie's Head on shutdown
     shake_head()
     sleep(0.25)  #  Give head time to get centered.
-    my_gopigo3.stop()  # Just in case. . .
+    my_gopigo3.stop()  #  Just in case. . .
     print("Charlie is signalling that shutdown has successfully completed by shaking his head.\n")
     sleep(0.25)
 
